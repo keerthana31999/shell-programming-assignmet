536a8034888f9559fa207587b1cc965b1492275a
keerthana31999 <keerthanacherry123@gmail.com>
2021-02-16 13:21:02 +0530

    Added:: function problems

---
+++ function/DegreeConversion.sh
@@ -0,0 +1,33 @@
+function farhenite() {
+		if [ $degC -ge 1 -a $degC -le 100 ]
+		then
+				degF=$((($degC*9/5)+32));
+		else
+				echo "Enter Temp in Range 0-100 degree Celsius"
+		fi
+}
+
+function celsius() {
+      if [ $degF -ge 32 -a $degF -le 212 ]
+      then
+            degC=$((($degF-32)*5/9));
+      else
+            echo "Enter Temp in Range 32-212 degree Farhenite"
+      fi
+}
+
+	read -p "1- Celsius to Farhenite\n 2- Farhenite to Celsius" choise
+
+case $choise in
+   1)
+		 read -p "Pass Value in Celsius :" degC
+         $( farhenite )
+         ;;
+   2)
+	    read -p "Pass Value in Farhenite :" degF
+         $( celsius )
+         ;;
+	*)
+			echo "pless chose right option"
+			;;
+esac
---
+++ function/PrimeNumbWithPalindrome.sh
@@ -0,0 +1,47 @@
+#!/bin/bash -x
+palindromeNumber(){
+        local number=$PrimeNum
+        local reverse=0
+        while [ $PrimeNum -gt 0 ]
+        do
+                a=`expr $PrimeNum % 10 `
+                PrimeNum=`expr $PrimeNum / 10 `
+                reverse=`expr $reverse \* 10 + $a`
+        done
+        echo $reverse
+        if [ $number -eq $reverse ]
+        then
+                echo Number $n is a prime and its Palindrome is also Prime
+        elif [ $number -ne $reverse -a $number -gt 0 ]
+        then
+		echo Number $n is a Prime but its Palindrome is not a Prime
+	else
+		echo Number is Not a Prime Number
+        fi
+}
+
+
+function primeNumber() {
+number1=0
+for (( num=1; num<=$n; num++ ))
+do
+	if [ $(($n%$num)) -eq 0 ]
+	then
+		((number1++))
+	fi
+done
+
+if [ $number1 -eq 2 ]
+then
+	echo $n
+else
+	echo -1
+fi
+
+}
+
+read -p "Enter Number To Check Prime of Not :" n
+
+PrimeNum=$( primeNumber $n )
+
+PalindromeNum=$( palindromeNumber $PrimeNum )
